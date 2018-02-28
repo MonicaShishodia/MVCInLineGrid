be20f067488ae2072ec6b530882102344ea0a30d
Monika <monika@ericsson.com>
2018-02-28 23:30:53 +0530

    added auto complete

--- MVC_Inline_Grid/MVC_Inline_Grid/Controllers/DummyController.cs
+++ MVC_Inline_Grid/MVC_Inline_Grid/Controllers/DummyController.cs
@@ -9,7 +9,10 @@ namespace MVC_Inline_Grid.Controllers
 {
     public class DummyController : Controller
     {
-        List<DummyModel> lst = new List<DummyModel> { new DummyModel() { ID = 1, FirstName = "John", LastName = "Doe" } };
+        List<DummyModel> lst = new List<DummyModel> { new DummyModel() { ID = 1, FirstName = "John", LastName = "Doe" },
+         new DummyModel() { FirstName = "Jane", LastName = "Smith", ID = 1 },
+         new DummyModel() { FirstName = "Jennifer", LastName = "Carter", ID = 3 },
+         new DummyModel() { FirstName = "Jack", LastName = "Smith", ID = 4 }};
 
         // GET: Dummy
         public ActionResult Index()
@@ -22,15 +25,28 @@ namespace MVC_Inline_Grid.Controllers
             return View(lst);
         }
         [HttpPost]
-        public ActionResult EditDummyData(int ID,string FirstName,string LastName)
+        public ActionResult EditDummyData(int ID, string FirstName, string LastName)
         {
             if (lst.Where(x => x.ID == ID).FirstOrDefault() != null)
             {
                 lst.Where(x => x.ID == ID).First().FirstName = FirstName;
                 lst.Where(x => x.ID == ID).First().LastName = LastName;
-                return Json(lst,JsonRequestBehavior.AllowGet);
+                return Json(lst, JsonRequestBehavior.AllowGet);
             }
             else return null;
+        }
+        [HttpPost]
+        public JsonResult AutoComplete(string prefix)
+        {
+            var names = (from name in lst
+                         where name.FirstName.StartsWith(prefix)
+                         select new
+                         {
+                             label = name.FirstName,
+                             val = name.ID
+                         }).ToList();
+
+            return Json(names);
         }
     }
 }
